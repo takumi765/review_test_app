25d2372e5009564626f776f4367235fa9c6ff09f
Takumi765 <82143606+takumi765@users.noreply.github.com>
2022-10-14 01:00:28 +0900

    add urls

--- test/urls.py
+++ test/urls.py
@@ -1,6 +1,11 @@
 from django.urls import path
 from . import views
 
+app_name='test'
+
 urlpatterns = [
-    path("", views.index, name="test"),
+    path("", views.index, name="index"),
+    path("create/", views.create, name="create"),
+    path("exam/", views.exam, name="exam"),
+    path("history/", views.history, name="history"),
 ]
--- test/views.py
+++ test/views.py
@@ -3,13 +3,33 @@ from .models import Test
 
 def index(request):
   if request.method == 'GET':
-    tests_list = {}
-    tests = Test.objects.all()
-    tests_list["tests_list"] = tests
-    return render(request, 'index.html', tests_list)
-
+    return render(request, 'index.html', )
   elif request.method == 'POST':
     question = request.POST['question']
     answer = request.POST['answer']
     Test.objects.create(que=question, ans=answer)
-    return redirect('test')
+    return redirect('index.html')
+
+def create(request):
+  if request.method == 'GET':
+    return render(request, 'create.html')
+  elif request.method == 'POST':
+    question = request.POST['question']
+    answer = request.POST['answer']
+    Test.objects.create(que=question, ans=answer)
+    return render(request, 'create.html')
+
+def exam(request):
+  if request.method == 'GET':
+    return render(request, 'exam.html')
+  elif request.method == 'POST':
+    return render(request, 'exam.html')
+
+def history(request):
+  if request.method == 'GET':
+    tests_list = {}
+    tests = Test.objects.all()
+    tests_list["tests_list"] = tests
+    return render(request, 'history.html', tests_list)
+  elif request.method == 'POST':
+    return render(request, 'history.html')
