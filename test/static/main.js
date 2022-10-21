a136fc1f5a4c4d11f1cbdf58e57a3c62b9a9aed4
Takumi765 <82143606+takumi765@users.noreply.github.com>
2022-10-21 18:15:37 +0900

    not change

--- test/static/main.js
+++ test/static/main.js
@@ -20,3 +20,10 @@ new Vue({
     },
   }
 })
+
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
+})
